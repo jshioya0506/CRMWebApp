cdf3ac4b9e60cd3148f639aa765e15ae66b2558a
jshioya0506 <34739536+jshioya0506@users.noreply.github.com>
2018-01-24 15:17:26 +0900

    新規追加。

---
+++ src/main/java/jp/co/nexus/LoginController.java
@@ -0,0 +1,28 @@
+package jp.co.nexus;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "login";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>N-CRM</title>
+<link href='https://fonts.googleapis.com/css?family=Alef:700,400'
+	rel='stylesheet' type='text/css'>
+<meta name="viewport"
+	content="initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+	<form>
+		<h1>Login</h1>
+		<input type="text" placeholder="E-mail Address" /> 
+		<input type="password" placeholder="Password" />
+		<button>Login</button>
+	</form>
+	<script
+		src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+</body>
+</html>
