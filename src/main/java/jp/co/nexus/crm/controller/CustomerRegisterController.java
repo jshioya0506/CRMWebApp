d203ec1aaeeb0b7bc98aecaabc842f426e81ba2a
jshioya <shioya@nexus-nt.co.jp>
2018-03-07 17:25:16 +0900

    コントローラ追加

---
+++ src/main/java/jp/co/nexus/crm/controller/CustomerRegisterController.java
@@ -0,0 +1,33 @@
+package jp.co.nexus.crm.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author jshioya
+ *
+ */
+@Controller
+public class CustomerRegisterController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomerRegisterController.class);
+	
+	/**
+	 * 
+	 * @param locale
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String doProcess(Locale locale, Model model) {
+		return "login";
+	}
+	
+}
