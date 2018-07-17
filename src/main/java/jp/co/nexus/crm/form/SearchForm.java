e182f43e6248f0feb6da4778c5f33bb920a4030e
s_tanioka <s_tanioka@nexus-nt.co.jp>
2018-07-18 00:15:32 +0900

    検索機能の追加

--- src/main/java/jp/co/nexus/crm/form/SearchForm.java
+++ src/main/java/jp/co/nexus/crm/form/SearchForm.java
@@ -6,12 +6,14 @@ package jp.co.nexus.crm.form;
  *
  */
 public class SearchForm {
-	
+
 	/** 職員コード */
 	private String staffCode;
-	/** 顧客コード */
+	/**  */
 	private String customerCode;
-	
+	/** 顧客コード */
+	private String companies;
+
 	/**
 	 * 職員コードを取得
 	 * @return 顧客コード
@@ -19,7 +21,7 @@ public class SearchForm {
 	public String getStaffCode() {
 		return staffCode;
 	}
-	
+
 	/**
 	 * 職員コードを設定
 	 * @param staffCode 職員コード
@@ -27,22 +29,38 @@ public class SearchForm {
 	public void setStaffCode(String staffCode) {
 		this.staffCode = staffCode;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return
 	 */
 	public String getCustomerCode() {
 		return customerCode;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param customerCode
 	 */
 	public void setCustomerCode(String customerCode) {
 		this.customerCode = customerCode;
 	}
-	
-	
+
+	/**
+	 * 顧客コードを取得
+	 * @return 顧客コード
+	 */
+	public String getcompanies() {
+		return companies;
+	}
+
+	/**
+	 * 顧客コードを設定
+	 * @param companies 顧客コード
+	 */
+	public void setcompanies(String companies) {
+		this.companies = companies;
+	}
+
+
 }
