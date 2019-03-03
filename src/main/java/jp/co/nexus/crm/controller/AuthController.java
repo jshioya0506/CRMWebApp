cee87d849a65906711cebe85338dbd2e749a05ff
渡邉文弘 <@192.168.0.4>
2019-03-04 02:29:50 +0900

    コミットとプッシュのテスト

--- src/main/java/jp/co/nexus/crm/controller/AuthController.java
+++ src/main/java/jp/co/nexus/crm/controller/AuthController.java
@@ -1,7 +1,5 @@
 package jp.co.nexus.crm.controller;
 
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,47 +7,39 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.nexus.crm.facade.AuthFacade;
 import jp.co.nexus.crm.facade.CustomersFacade;
-import jp.co.nexus.crm.form.LoginForm;
+import jp.co.nexus.crm.form.SearchForm;
 
 /**
- * 認証処理を行うコントローラ
+ * 顧客情報絞込検索のコントローラ
  * @author jshioya
  *
  */
 @Controller
 public class AuthController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-	
+
 	/**
-	 * 認証処理を行い、顧客情報を抽出したデータを顧客一覧画面へ表示
-	 * @param loginForm ログインフォームの入力データ
-	 * @param model SpringFrameworkのモデルクラス
-	 * @return 正常終了時にcustomer_list.jspへ遷移
+	 * 担当者、社名で検索した顧客情報を顧客一覧画面へ表示
+	 * @param form 検索条件の入力情報
+	 * @param model SpringFrameworkのモデル
+	 * @return 正常/終了時もcustomer_list.jspへ遷移
 	 */
-	@RequestMapping(value = "/auth", method = { RequestMethod.GET, RequestMethod.POST })
-	public String doProcess(LoginForm loginForm, Model model) {
-		
-		// ユーザID（メールアドレス)
-		String userId = loginForm.getUserId();
-		//　パスワード
-		String password = loginForm.getPassword();
-		
-		// ログイン認証(認証に失敗した場合はfalseが返る)
-		AuthFacade authFacade = new AuthFacade();
-		boolean doneLogin = authFacade.doLogin(userId, password);
-		if (!doneLogin) {
-			//FIXME エラー処理(ログイン画面にエラーメッセージを通知)
-			return "login";
-		}
-		
-		// 顧客テーブルからデータを抽出し、画面に表示
+	@RequestMapping(value = "/searchCustomer", method = RequestMethod.GET)
+	public String doProcess(SearchForm form, Model model) {
+
+		// １．[担当者]の入力データを取得
+		String staffCode = form.getStaffCode();
+		// 2.[社名]の入力データを取得
+		String companyCode = form.getCustomerCode();
+		// 3.担当者、社名で絞込検索
+
+		// 顧客テーブルから検索条件のデータを抽出し、画面に表示
 		CustomersFacade customerFacade = new CustomersFacade();
-		customerFacade.doAction(model);
-		
+		customerFacade.doSearch(model, staffCode, companyCode);
+
 		return "customer_list";
 	}
-	
+
 }
